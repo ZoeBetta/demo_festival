b49f3e5f26651eb90c2c58280a7427e7383ebd26
ZoeBetta <77151364+ZoeBetta@users.noreply.github.com>
2023-10-26 08:50:01 -0400

    linea

---
+++ scripts/linea.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+#--------Include modules---------------
+from ast import Is
+from copy import copy
+import rospy
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+from nav_msgs.msg import OccupancyGrid, Path
+from geometry_msgs.msg import PointStamped, PoseStamped 
+import tf
+from numpy import array,vstack,delete
+from sklearn.cluster import MeanShift
+from rrt_exploration.msg import PointArray
+from shapely.geometry import Point as pt
+from shapely.geometry.polygon import Polygon
+from shapely.geometry import  LineString
+from zoe_interface.msg import fiducial
+from rrt_exploration.srv import Floor, FloorRequest
+import time
+
+# Subscribers' callbacks------------------------------
+
+
+# Node----------------------------------------------
+def node():
+	rospy.init_node('filter', anonymous=False)
+	# fetching all parameters
+	rate=rospy.Rate(1)
+	pub= rospy.Publisher('goaltospot', PoseStamped, queue_size=10)
+	goal= PoseStamped()
+	time.sleep(2)
+	print("dovrebbe fare cose ora")
+	goal.pose.position.y=1.5
+	pub.publish(goal)
+	print("pubblicato 1.5")
+	time.sleep(10)
+	goal.pose.position.y=-1.0
+	pub.publish(goal)
+
+if __name__ == '__main__':
+	try:
+		node()
+	except rospy.ROSInterruptException:
+		pass
+ 
+ 
+ 
+ 
